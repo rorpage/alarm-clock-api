b4249890f57a33bc91e7ec0662fdd90098b80069
Robbie Page <robbie.page@e-gineering.com>
2020-02-21 08:51:59 -0500

    swap hardcoded latlon to envvars

--- api/info.js
+++ api/info.js
@@ -1,5 +1,5 @@
-import redis from "async-redis"
-import fetch from "node-fetch"
+import redis from 'async-redis';
+import fetch from 'node-fetch';
 
 export default async (req, res) => {
   let response = processDateAndTime();
@@ -18,15 +18,15 @@ export default async (req, res) => {
       const weatherApiKey = process.env.WEATHER_API_KEY || '';
 
       await fetch(
-        `https://api.darksky.net/forecast/${weatherApiKey}/39.7707286,-86.0703977?exclude=minutely,hourly,alerts,flags`
+        `https://api.darksky.net/forecast/${weatherApiKey}/${latitude},${longitude}?exclude=minutely,hourly,alerts,flags`
       )
-      .then((res) => res.json())
-      .then((json) => {
-        response = formatWeather(response, json);
+        .then(res => res.json())
+        .then(json => {
+          response = formatWeather(response, json);
 
-        redis_client.setex(key, weather_time_to_live, JSON.stringify(json));
-        redis_client.quit();
-      });
+          redis_client.setex(key, weather_time_to_live, JSON.stringify(json));
+          redis_client.quit();
+        });
     } else {
       const cached_data = JSON.parse(redis_data);
       response = formatWeather(response, cached_data);
@@ -37,28 +37,31 @@ export default async (req, res) => {
     console.log(error);
   }
 
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
 
   res.json(response);
-}
+};
 
 function processDateAndTime() {
   let response = {};
 
-  const dayNames = [ "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" ];
+  const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
   const utc_offset = -5;
 
   const client_date = new Date();
-  const utc = client_date.getTime() + (client_date.getTimezoneOffset() * 60000);
-  const server_date = new Date(utc + (3600000 * utc_offset));
+  const utc = client_date.getTime() + client_date.getTimezoneOffset() * 60000;
+  const server_date = new Date(utc + 3600000 * utc_offset);
 
   const server_hours = server_date.getHours();
-  const hour = (server_hours + 11) % 12 + 1;
-  const hour_display = (hour < 10) ? `0${hour}` : hour;
+  const hour = ((server_hours + 11) % 12) + 1;
+  const hour_display = hour < 10 ? `0${hour}` : hour;
   const minutes = server_date.getMinutes();
-  const minutes_display = (minutes < 10) ? `0${minutes}` : minutes;
+  const minutes_display = minutes < 10 ? `0${minutes}` : minutes;
   const time = `${hour_display}:${minutes_display}`;
 
   const time_digits = [];
@@ -71,9 +74,9 @@ function processDateAndTime() {
   response.time_digits = time_digits;
 
   const month = server_date.getMonth() + 1;
-  const month_display = (month < 10) ? `0${month}` : month;
+  const month_display = month < 10 ? `0${month}` : month;
   const day = server_date.getDate();
-  const day_display = (day < 10) ? `0${day}` : day;
+  const day_display = day < 10 ? `0${day}` : day;
   const date = `${month_display}/${day_display}`;
 
   response.date = date;
@@ -81,9 +84,9 @@ function processDateAndTime() {
   const day_of_week = dayNames[server_date.getDay()];
   response.day_of_week = day_of_week;
 
-  response.line_1 = `Today is ${day_of_week}, ${date}`
+  response.line_1 = `Today is ${day_of_week}, ${date}`;
 
-  response.brightness = (server_hours > 7 && server_hours < 20) ? 255 : 100;
+  response.brightness = server_hours > 7 && server_hours < 20 ? 255 : 100;
 
   return response;
 }
@@ -91,11 +94,12 @@ function processDateAndTime() {
 function formatWeather(response, json) {
   const temperature = Math.round(json.currently.temperature);
   const currently = json.currently.summary.toLowerCase();
-  const currentlyFormatted = currently.charAt(0).toUpperCase() + currently.substring(1);
+  const currentlyFormatted =
+    currently.charAt(0).toUpperCase() + currently.substring(1);
 
   response.temperature = temperature;
   response.currently = currentlyFormatted;
-  response.line_2 = `${currentlyFormatted}, ${temperature}F`
+  response.line_2 = `${currentlyFormatted}, ${temperature}F`;
 
   return response;
 }
